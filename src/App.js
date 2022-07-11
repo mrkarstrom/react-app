344196657e63786aceb5482a769ae8fa5c0db8b0
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-07-11 11:49:31 +0200

    Returning to using the simpler jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
 
 function App() {
-  return React.createElement('div', { className: 'container' }, React.createElement('h1', {},'My App'))
+  return (
+    <div className='container'>
+        <h1>My App</h1>
+    </div>
+  )
 }
 
 export default App;
