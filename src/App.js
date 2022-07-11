b99d4ad2084410477985531100722c4333cbcff4
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-07-11 12:11:25 +0200

    Showing that javascript methods are working in the jsx

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ const body ='This is my blog post';
 
   return (
     <div className='container'>
-        <h1>{title}</h1>
+        <h1>{title.toUpperCase()}</h1>
         <p>{body}</p>
+        {Math.floor(Math.random() * 6 + 1)}
+
     </div>
   )
 }
