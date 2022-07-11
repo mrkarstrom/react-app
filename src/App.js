6eb5c4ca48092cddfde29075983e5a9eca2d6122
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-07-11 12:19:10 +0200

    Adding list output for the comments

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@
 function App() {
 const title='Blog Post';
 const body ='This is my blog post';
+const comments = [
+  {id:1, text:'Comment on one'},
+  {id:2, text:'Comment on two'},
+  {id:3, text:'Comment on three'}
+]
 
   return (
     <div className='container'>
         <h1>{title.toUpperCase()}</h1>
         <p>{body}</p>
-        {Math.floor(Math.random() * 6 + 1)}
 
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => (
+              <li>{comment.text}</li>
+            ) )} 
+          </ul>
+      </div>
     </div>
   )
 }
