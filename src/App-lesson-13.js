542d09b3255dff14248845ddc076ce7285a4a6de
Magnus Karström <magnus@yesweb.se>
2022-07-13 19:48:58 +0200

    Addex styling from Traversy index.css

---
+++ src/App-lesson-13.js
@@ -0,0 +1,15 @@
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+    {/* Use defaultProps in Header.jsx if there is nothing to send */}
+      <Header text= "Lets goo!" />  
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
