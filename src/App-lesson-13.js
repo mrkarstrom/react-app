429dd7a11951a63527fc6be36567726b50c568cd
Magnus Karström <magnus@yesweb.se>
2022-07-13 20:28:44 +0200

    A bug was hiding on line13 on Header.jsx - too many {}

--- src/App-lesson-13.js
+++ src/App-lesson-13.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
     {/* Use defaultProps in Header.jsx if there is nothing to send */}
-      <Header text= "Vue Mastery" bgColor='red' textColor='blue'/>  
+      <Header text="Vue Mastery" bgColor='red' textColor='blue'/>  
       <div className="container">
         <h1>My App</h1>
       </div>
