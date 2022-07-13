2a8dd14aa847f9fb42e21c6804bfc3aa81ee2cde
Magnus Karström <magnus@yesweb.se>
2022-07-13 06:08:38 +0200

    Using the chowComments const to display comments if its true

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-
 function App() {
-const title='Blog Post';
-const body ='This is my blog post';
-const comments = [
-  {id:1, text:'Comment on one'},
-  {id:2, text:'Comment on two'},
-  {id:3, text:'Comment on three'}
-]
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'Comment on one' },
+    { id: 2, text: 'Comment on two' },
+    { id: 3, text: 'Comment on three' },
+    { id: 4, text: 'Comment on four' },
+    { id: 5, text: 'Comment on five' },
+    { id: 6, text: 'Comment on six' },
+  ];
+
+  const loading = false;
+  const showComments = true;
+
+  if (loading) return <h1>Loading...</h1>;
 
   return (
-    <div className='container'>
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
 
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
+      {showComments && (
+        <div className="comments">
+          <h3>Comments ({comments.length})</h3>
           <ul>
             {comments.map((comment, index) => (
-              <li>{comment.text}</li>
-            ) )} 
+              <li key={index}>{comment.text}</li>
+            ))}
           </ul>
-      </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
 export default App;
