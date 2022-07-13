0bdc3808919d1adb69d52f405eec80cd14a02eea
Magnus Karström <magnus@yesweb.se>
2022-07-13 20:43:55 +0200

    Finished lesson 13

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,10 +21,14 @@ function Header({ text, bgColor, textColor}) {
 // Using defaultProps when no props (text) is sent
 Header.defaultProps = {
   text: 'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.6)',
+  textColor: '#41b883',
 };
 
 Header.propTypes = {
     text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor: PropTypes.string,
 }
 
 export default Header;
