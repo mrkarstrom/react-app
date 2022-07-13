abb6509b889bbc4c65244a5a991580334d963367
Magnus Karström <magnus@yesweb.se>
2022-07-13 19:18:20 +0200

    Setting defaultProps in Header to display if no text-prop is avail

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-function Header(props) {
+function Header({ text }) {
   return (
     <header>
       <div className="container">
-        <h2>{props.text}</h2>
+        <h2>{text}</h2>
       </div>
     </header>
   );
+}
+
+Header.defaultProps = {
+    text: 'Feedback UI',
 }
 
 export default Header;
