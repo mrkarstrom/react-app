e349da127c41fe04237d01c1b7d72af1f6de2f80
Magnus Karström <magnus@yesweb.se>
2022-07-20 00:24:40 +0200

    Added handleClick to put new state of Rating

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,12 +4,20 @@ function FeedbackItem() {
   const [rating, setRating] = useState(7)
   const [text, setText] = useState('This is the feedback!')
 
+  const handleClick = () => {
+    setRating((prev) => {
+      return prev + 1
+    })
+  }
+
+
   return (
     <div className='card'>
         <div className="num-display">{rating}</div>
         <div className="text-display">
           {text}
         </div>
+        <button onClick={handleClick}>Click</button>
     </div>
   )
 }
